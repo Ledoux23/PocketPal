c74ce0dc01cd055766144bd03fd2a9cb1b3730f7
TESSOH TENE <michel.tessohtene@cgi.com>
2024-02-20 15:26:02 +0100

    Initial commit

---
+++ pocket-pal/src/main/java/org/mtt/pocketpal/PocketPalApplication.java
@@ -0,0 +1,13 @@
+package org.mtt.pocketpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketPalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocketPalApplication.class, args);
+    }
+
+}
---
+++ pocket-pal/src/test/java/org/mtt/pocketpal/PocketPalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mtt.pocketpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketPalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
